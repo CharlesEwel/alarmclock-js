019f7b9f1e423ee2b5c325e24658973b81256813
Bang Nguyen and Charles Ewel <bang+charles@>
2016-08-02 10:56:42 -0700

    add turn off function

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,5 +1,7 @@
 var alarmClock = require('./../js/time.js').timeModule;
 
+var currentTimer;
+
 $(document).ready(function(){
   $('#time').text(moment());
 
@@ -7,23 +9,11 @@ $(document).ready(function(){
     event.preventDefault();
     var alarmTime = $("#alarm-time").val();
     var newAlarm = new alarmClock(alarmTime);
+    currentTimer = newAlarm.Timer();
+  });
 
-    var convertedAlarmTime = moment(alarmTime, "HH:mm a");
-    var convertedAlarmTimeHour = convertedAlarmTime.hour();
-    var convertedAlarmTimeMinute = convertedAlarmTime.minute();
-
-    // var minutesToAlarm = 60*(convertedAlarmTimeHour-currentTimeHour)+(convertedAlarmTimeMinute-currentTimeMinute);
-    // var millisecondsToAlarm=60000*minutesToAlarm;
-    setInterval(function(){
-        var currentTimeHour = moment().hour();
-        var currentTimeMinute = moment().minute();
-        if(currentTimeHour===convertedAlarmTimeHour && convertedAlarmTimeMinute===currentTimeMinute)
-        {
-          $("#test").text("alarm is going off");
-        }
-      }
-      , 10000);
-
-
+  $("#turn-off-alarm").click(function(event){
+    clearInterval(myTimer)
+    $("#test").hide();
   });
 });
--- js/time.js
+++ js/time.js
@@ -2,4 +2,22 @@ function alarmClock(alarmTime) {
   this.alarmTime = alarmTime;
 }
 
+alarmClock.prototype.Timer = function(){
+  var convertedAlarmTime = moment(this.alarmTime, "HH:mm a");
+  var convertedAlarmTimeHour = convertedAlarmTime.hour();
+  var convertedAlarmTimeMinute = convertedAlarmTime.minute();
+
+  myTimer=setInterval(function(){
+      var currentTimeHour = moment().hour();
+      var currentTimeMinute = moment().minute();
+      if(currentTimeHour>=convertedAlarmTimeHour && currentTimeMinute>=convertedAlarmTimeMinute)
+      {
+        $("#test").text("alarm is going off");
+      }
+      console.log("interval is running")
+    }
+    , 10000);
+  myTimer;
+}
+
 exports.timeModule = alarmClock;
