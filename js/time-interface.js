e220950426e4deb891606ccac6dadd5e3826eeaa
Bang Nguyen and Charles Ewel <bang+charles@>
2016-08-02 15:10:45 -0700

    attempt to refactor to allow multiple alarms

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,27 +1,38 @@
 var alarmClock = require('./../js/time.js').timeModule;
+var numberOfAlarms=0;
 
 $(document).ready(function(){
   $('#time').text(moment());
 
-  $("#set-alarm").submit(function(){
+  $(".set-alarm").submit(function(event){
     event.preventDefault();
-    var alarmTime = $("#alarm-time").val();
-    var newAlarm = new alarmClock(alarmTime);
-    currentTimer = newAlarm.Timer();
+    var alarmNumber=$(".alarm-number").val();
+    var alarmTime = $(".alarm-time").val();
+    console.log(alarmTime);
+    var newAlarm = new alarmClock(alarmTime, alarmNumber);
+    console.log(newAlarm.alarmTime)
+    newAlarm.Timer();
   });
 
-  $("#snooze").submit(function(event) {
+  $(".snooze").submit(function(event) {
     event.preventDefault();
+    var localAlarmNumber=$(this).attr('alarmNumber');
     clearInterval(myTimer);
-    $("#test").hide();
-    var snoozeLength = $("#snooze-options").val();
+    $("div[alarmNumber='"+localAlarmNumber+"']").hide();
+    var snoozeLength = $(".snooze-options").val();
     var alarmTime = moment().add(snoozeLength, 'minutes');
-    var newAlarm = new alarmClock(alarmTime);
+    var newAlarm = new alarmClock(alarmTime, localAlarmNumber);
     currentTimer = newAlarm.Timer();
   });
 
-  $("#turn-off-alarm").click(function(event){
+  $(".turn-off-alarm").click(function(event){
+    var localAlarmNumber=$(this).attr('alarmNumber');
     clearInterval(myTimer);
-    $("#test").hide();
+    $("div[alarmNumber='"+localAlarmNumber+"']").hide();
+  });
+  $("#another-alarm").click(function(event){
+    numberOfAlarms++;
+    var stringifiedAlarmForm = "<form class='form-group set-alarm'><input type='hidden' class='alarm-number' value='"+numberOfAlarms+"'><label for='alarm-time'>Alarm Time:</label> <input type='time' class='alarm-time' required><button type='submit'>Set Alarm</button></form><div class='output' alarmNumber='"+numberOfAlarms+"'><img src='img/alarm_clock.png' alt='ringing alarm clock'></div><form class='snooze'><label for='snooze-options'>How Many Minutes to Snooze?</label><input type='number' class='snooze-options' value='2'><button type='submit'>Snooze</button></form><button type='button' alarmNumber='"+numberOfAlarms+"' class='turn-off-alarm'>Turn Off Alarm</button>";
+    $("#alarm-forms").append(stringifiedAlarmForm);
   });
 });
--- js/time.js
+++ js/time.js
@@ -1,18 +1,22 @@
-function alarmClock(alarmTime) {
-  this.alarmTime = alarmTime;
+function alarmClock(alarmTime, alarmNumber) {
+  this.alarmTime=alarmTime;
+  this.alarmNumber=alarmNumber;
+  console.log('is it working?');
 }
 
 alarmClock.prototype.Timer = function(){
-  var convertedAlarmTime = moment(this.alarmTime, "HH:mm a");
+  var convertedAlarmTime = moment(this.alarmTime, "HH:mm");
   var convertedAlarmTimeHour = convertedAlarmTime.hour();
   var convertedAlarmTimeMinute = convertedAlarmTime.minute();
-
+  var specificAlarm = this.alarmNumber;
   myTimer=setInterval(function(){
+    console.log("Iterating")
       var currentTimeHour = moment().hour();
       var currentTimeMinute = moment().minute();
       if(currentTimeHour>=convertedAlarmTimeHour && currentTimeMinute>=convertedAlarmTimeMinute)
       {
-        $("#test").show();
+        console.log("Should be ringing")
+        $("div[alarmNumber='"+specificAlarm+"']").show();
       }
     }
     , 10000);
